ce493d10332cb9b4c9cebda59e335c187a5ec7f0
Azin Asili <azinasili@gmail.com>
2017-06-18 13:53:04 -0400

    moved logic to parent. using props instead

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -2,31 +2,13 @@ import React, { Component } from 'react';
 import './Search.css';
 
 class Search extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: '',
-    }
-    this.searchUser = this.searchUser.bind(this);
-    this.setUser = this.setUser.bind(this);
-  }
-
-  searchUser(event) {
-    event.preventDefault();
-    console.log(this.state);
-  }
-
-  setUser(event) {
-    this.setState({user: event.target.value});
-  }
-
   render() {
     return (
-      <form onSubmit={this.searchUser} className="Search">
+      <form onSubmit={this.props.searchUser} className="Search">
         <div className="Input-group">
           <p className="Input-field">
             <label className="Input-label sr-only" htmlFor="profileSearch">Profile Search</label>
-            <input type="text" id="profileSearch" name="profileSearch" className="Input" placeholder="Profile Search" value={this.state.user} onChange={this.setUser}  />
+            <input type="text" id="profileSearch" name="profileSearch" className="Input" placeholder="Profile Search" onChange={this.props.setUser}  />
           </p>
           <button className="Input-button">Search</button>
         </div>
