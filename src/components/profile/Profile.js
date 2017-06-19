49a38e6db6a432b19c74b2fb9a1cbe6f8397943b
Azin Asili <azinasili@gmail.com>
2017-06-19 16:07:42 -0400

    added conditional rendering. This seems like the worst way to do it, but idk

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -3,39 +3,49 @@ import './Profile.css';
 
 class Profile extends Component {
   render() {
+    let avatar = this.props.profile.avatar_url ? <img src={this.props.profile.avatar_url} alt="Github profile avatar" className="Profile-image" /> : null;
+    let name = this.props.profile.name ? <p>{this.props.profile.name}</p> : null;
+    let login = this.props.profile.login ? <p className="Profile-meta-item">{this.props.profile.login}</p> : null;
+    let blog = this.props.profile.blog ? <p className="Profile-meta-item"><a href={this.props.profile.blog} className="Profile-meta-value">{this.props.profile.blog}</a></p> : null;
+    let company = this.props.profile.company ? <p className="Profile-meta-item">{this.props.profile.company}</p> : null;
+    let location = this.props.profile.location ? <p>{this.props.profile.location}</p> : null;
+    let bio = this.props.profile.bio ? <p>{this.props.profile.bio}</p> : null;
+    let repos = this.props.profile.public_repos ? <p className="Profile-meta-item"><span className="Profile-meta-value">{this.props.profile.public_repos}</span><span className="Profile-meta-title">Repos</span></p> : null;
+    let gists = this.props.profile.public_gists ? <p className="Profile-meta-item"><span className="Profile-meta-value">{this.props.profile.public_gists}</span><span className="Profile-meta-title">Gists</span></p> : null;
+    let followers = this.props.profile.followers ? <p className="Profile-meta-item"><span className="Profile-meta-value">{this.props.profile.followers}</span><span className="Profile-meta-title">Followers</span></p> : null;
+    let following = this.props.profile.following ? <p className="Profile-meta-item"><span className="Profile-meta-value">{this.props.profile.following}</span><span className="Profile-meta-title">Following</span></p> : null;
+
     return (
       <div className="Profile">
-        <img src={this.props.profile.avatar_url} alt="Github profile avatar" className="Profile-image" />
-        <div className="Profile-content">
-          <p>{this.props.profile.name}</p>
-          <div className="Profile-meta">
-            <p className="Profile-meta-item">{this.props.profile.login}</p>
-            <p className="Profile-meta-item">
-              <a href={this.props.profile.blog} className="Profile-meta-value">{this.props.profile.blog}</a>
-            </p>
-            {this.props.profile.company ? <p className="Profile-meta-item">{this.props.profile.company}</p> : ''}
-          </div>
-          <p className="Profile-meta-item">{this.props.profile.location}</p>
-          <p>{this.props.profile.bio}</p>
-          <div className="Profile-meta">
-            <p className="Profile-meta-item">
-              <span className="Profile-meta-value">{this.props.profile.public_repos}</span>
-              <span className="Profile-meta-title">Repos</span>
-            </p>
-            <p className="Profile-meta-item">
-              <span className="Profile-meta-value">{this.props.profile.public_gists}</span>
-              <span className="Profile-meta-title">Gists</span>
-            </p>
-            <p className="Profile-meta-item">
-              <span className="Profile-meta-value">{this.props.profile.followers}</span>
-              <span className="Profile-meta-title">Followers</span>
-            </p>
-            <p className="Profile-meta-item">
-              <span className="Profile-meta-value">{this.props.profile.following}</span>
-              <span className="Profile-meta-title">Following</span>
-            </p>
-          </div>
-        </div>
+        {avatar}
+        {
+          name ? (
+            <div className="Profile-content">
+              {name}
+              {
+                login || blog || company ? (
+                  <div className="Profile-meta">
+                    {login}
+                    {blog}
+                    {company}
+                  </div>
+                ) : null
+              }
+              {location}
+              {bio}
+              {
+                repos || gists || followers || following ? (
+                  <div className="Profile-meta">
+                    {repos}
+                    {gists}
+                    {followers}
+                    {following}
+                  </div>
+                ) : null
+              }
+            </div>
+          ) : null
+        }
       </div>
     );
   }
