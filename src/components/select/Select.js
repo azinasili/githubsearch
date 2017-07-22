9da5d23506cc054a3561e759aa8edf24716aa633
Azin Asili <azinasili@gmail.com>
2017-07-22 14:41:33 -0400

    made select super dumb component

--- src/components/select/Select.js
+++ src/components/select/Select.js
@@ -2,23 +2,13 @@ import React, { Component } from 'react';
 import './Select.css';
 
 class Select extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      switcherOptions: [this.props.selectValue, 20, 30],
-    }
-  }
-
   render() {
     return (
       <label className="Select">
         <span className="Select-label">Show:</span>
-        <select
-          className="Select-input"
-          value={this.props.selectValue}
-          onChange={this.props.handleSelect}>
+        <select className="Select-input" value={this.props.selectValue} onChange={this.props.handleSelect}>
           {
-            this.state.switcherOptions.map((option) => {
+            this.props.selectOptions.map((option) => {
               return (
                 <option
                   className="Select-option"
--- src/containers/panel/Panel.js
+++ src/containers/panel/Panel.js
@@ -7,7 +7,8 @@ class Panel extends Component {
     super(props);
     this.state = {
       title: this.props.title,
-      selectValue: 10,
+      selectValue: null,
+      selectOptions: [10, 20, 30],
     }
     this.handleSelect = this.handleSelect.bind(this);
   }
@@ -17,12 +18,17 @@ class Panel extends Component {
   }
 
   render() {
+    let selectValue = this.state.selectValue ? this.state.selectValue : this.state.selectOptions[0];
+
     return (
       <div className="Panel">
         <div className="Panel-header">
-          <h2 className="Panel-title">Top {this.state.selectValue} {this.state.title}</h2>
+          <h2 className="Panel-title">Top {selectValue} {this.state.title}</h2>
           <form className="Panel-switcher">
-            <Select selectValue={this.state.selectValue} handleSelect={this.handleSelect} />
+            <Select
+              selectValue={this.state.selectValue}
+              selectOptions={this.state.selectOptions}
+              handleSelect={this.handleSelect} />
           </form>
         </div>
         <div className="Panel-body">
