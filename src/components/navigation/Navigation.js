eb1d4f316ada8c55518fe2ffaa63d0fa86efd457
Azin Asili <azinasili@gmail.com>
2017-08-25 08:40:57 -0400

    new nav for footr

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 import './Navigation.css';
 
 class Navigation extends Component {
   render() {
+    const CLASSES = classNames({
+      'Navigation-button': true,
+    });
+    console.log(this.props);
+
     return (
       <nav className="Navigation">
         <ul className="Navigation-list">
-          <li className="Navigation-listitem">Users</li>
-          <li className="Navigation-listitem">Repos</li>
+          <li className="Navigation-listitem"><button onClick={this.props.switchUserView} className="Navigation-button">Users</button></li>
+          <li className="Navigation-listitem"><button onClick={this.props.switchRepoView} className="Navigation-button">Repos</button></li>
         </ul>
       </nav>
     );
   }
 }
+
+Navigation.propTypes = {
+  active: PropTypes.bool,
+  switchUserView: PropTypes.func,
+  switchRepoView: PropTypes.func,
+};
 
 export default Navigation;
--- src/containers/footer/Footer.js
+++ src/containers/footer/Footer.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { switchToUsers, switchToRepos } from '../../actions/switchMobileView';
 import Nav from '../../components/navigation/Navigation';
 import './Footer.css';
 
 class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.switchUserView = this.switchUserView.bind(this);
+    this.switchRepoView = this.switchRepoView.bind(this);
+  }
+
+  switchUserView(event) {
+    event.preventDefault();
+    this.props.dispatch(switchToUsers());
+  }
+
+  switchRepoView(event) {
+    event.preventDefault();
+    this.props.dispatch(switchToRepos());
+  }
+
   render() {
     return (
       <footer className="Footer">
-        <Nav />
+        <Nav
+        active={this.props.userView ? this.props.userView : this.props.repoView}
+        switchUserView={this.switchUserView}
+        switchRepoView={this.switchRepoView} />
       </footer>
     );
   }
 }
 
-export default Footer;
+
+function mapStateToProps(state) {
+  return {
+    userView: state.mobileView.users,
+    repoView: state.mobileView.repos,
+  };
+}
+
+export default connect(mapStateToProps)(Footer);
