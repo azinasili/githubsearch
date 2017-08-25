3cb15be2c02b1f9a0a45892f69f8b80cdee858c1
Azin Asili <azinasili@gmail.com>
2017-08-25 16:30:10 -0400

    updated action for mobile view

--- src/actions/switchMobileView.js
+++ src/actions/switchMobileView.js
@@ -1,11 +1,13 @@
-export function switchToUsers() {
+export function switchToUsers(id) {
   return {
     type: 'SWITCH_TO_USER',
+    payload: id,
   };
 }
 
-export function switchToRepos() {
+export function switchToRepos(id) {
   return {
     type: 'SWITCH_TO_REPOS',
+    payload: id,
   };
 }
